3f5040948bd4f8c9c5b2d1939d9b5c99c3ee205f
s262800 <146329948+s262800@users.noreply.github.com>
2023-11-16 10:43:12 +0000

    retry button fix

---
+++ Assets/Scripts/DelayPulseAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayPulseAnimation : MonoBehaviour
+{
+    public Animator anim;
+
+    void Start()
+    {
+        anim.SetBool("CanPlay", false);
+      //  StartCoroutine(Delay());
+    }
+
+ 
+
+    public void Appear()
+    {
+        anim.SetBool("CanPlay", true);
+
+    }
+}
+
